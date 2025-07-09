21248d4a9de472c5d66a7c8ae17e2acc3f246b6d
Ianukito <81605985+Ianuki@users.noreply.github.com>
2025-07-09 02:16:40 -0300

    Update defines.h

--- Headers/defines.h
+++ Headers/defines.h
@@ -1,1 +1,11 @@
-#define MAX_LINE_LENGTH 30
+/* Modular Definitions */
+
+#define LAZIC_VERSION 1
+#define MAX_LINE_LENGTH 50
+#define MAX_LINES 50
+#define COLOR "NO"
+#define AKA "YES"
+
+/* Strict Definitions -> Avoid modifications */
+
+#define COMMAND_MAX_LENGTH 10
